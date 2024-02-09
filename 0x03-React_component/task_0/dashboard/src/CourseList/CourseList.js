85a3d8fb13102dbb3eaa5a06e8348361f2933c31
OmarTl2005 <hiba41145@gmail.com>
2024-02-09 14:13:15 +0100

    done

--- 0x03-React_component/task_0/dashboard/src/CourseList/CourseList.js
+++ 0x03-React_component/task_0/dashboard/src/CourseList/CourseList.js
@@ -6,21 +6,19 @@ import CourseShape from "./CourseShape";
 
 function CourseList({ listCourses }) {
   return (
-    <div className="table-div">
-      <table id="CourseList">
-        <thead>
-          <CourseListRow textFirstCell="Available courses" isHeader={true} />
-          <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-        </thead>
-        <tbody>
-          {listCourses.length > 0 ? (
-            listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
-          ) : (
-            <CourseListRow textFirstCell="No course available yet" />
-          )}
-        </tbody>
-      </table>
-    </div>
+    <table id="CourseList">
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length > 0 ? (
+          listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
+        ) : (
+          <CourseListRow textFirstCell="No course available yet" />
+        )}
+      </tbody>
+    </table>
   );
 }
 
